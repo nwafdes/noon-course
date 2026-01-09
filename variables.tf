c3f7ceda5ff34eca5f1842ed82c7ef775983095a
unknown <nwafdes@gmail.com>
2026-01-09 06:51:41 +0300

    ➕🔔 : Updated html file

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "me-south-1"
+  default     = "us-east-1"
 }
 
 variable "bucket_name" {
